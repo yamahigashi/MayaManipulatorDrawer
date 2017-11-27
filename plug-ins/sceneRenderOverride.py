c1dcc1a0836e2219c364e827a1d15089bd577537
yamahigashi <yamahigashi@gmail.com>
2017-11-27 18:54:30 +0900

    gardening.

--- plug-ins/sceneRenderOverride.py
+++ plug-ins/sceneRenderOverride.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-'''Custom Render Override for drawnig Manipulator even during playback.'''
 """Custom Render Override for drawnig Manipulator even during playback."""
 ###############################################################################
 import sys
